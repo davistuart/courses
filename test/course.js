78470869962479cfacd027fd780f74214e389bab
Rodrigo Elizeu Goncalves <rodrigoegoncalves@gmail.com>
2014-03-15 17:55:19 -0300

    Creating some test examples

--- test/course.js
+++ test/course.js
@@ -7,50 +7,82 @@ app     = require('../index.js');
 
 describe('create course', function () {
     it('should not create with invalid credentials', function (next) {
+        // TODO
         next();
     });
 
     it('should create with valid credentials', function (next) {
-        next();
+        var course = {
+            "id": "42"
+            , "name": "Ciência da Computação"
+            , "level": "G"
+            , "area": "Exatas"
+            , "qualification": "A"
+            , "credits": 200
+        };
+        request(app)
+            .post('/courses')
+            .send(course)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(error, response) {
+                if (error) {
+                    next(error);
+                } else {
+                    next();
+                }
+            });
     });
 });
 
 describe('list course', function () {
     it('should not list with invalid credentials', function (next) {
+        // TODO
         next();
     });
 
     it('should list', function (next) {
-        next();
+        request(app)
+            .get('/courses')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, next);
     });
 });
 
 describe('show course', function () {
     it('should not show with invalid credentials', function (next) {
+        // TODO
         next();
     });
 
     it('should show', function (next) {
+        // TODO
         next();
     });
 });
 
 describe('update course', function () {
     it('should not update with invalid credentials', function (next) {
+        // TODO
         next();
     });
 
     it('should update', function (next) {
+        // TODO
         next();
     });
 });
 
 describe('remove course', function () {
     it('should not remove with invalid credentials', function (next) {
+        // TODO
         next();
     });
 
     it('should remove', function (next) {
+        // TODO
         next();
     });
 });
