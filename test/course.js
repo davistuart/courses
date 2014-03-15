bbd326c69680c66c8bb7ca9b4da5f3768086e882
Rodrigo Elizeu Goncalves <rodrigoegoncalves@gmail.com>
2014-03-15 18:49:43 -0300

    Creating tests

--- test/course.js
+++ test/course.js
@@ -11,6 +11,47 @@ describe('create course', function () {
         next();
     });
 
+    it('should not create without required fields', function (next) {
+        var course = {};
+        request(app)
+            .post('/courses')
+            .send(course)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(500)
+            .end(function(error, response) {
+                if (error) {
+                    next(error);
+                } else {
+                    next();
+                }
+            });
+    });
+
+    it('should not create with invalid values', function (next) {
+        var course = {
+            "id": "42"
+            , "name": "Ciência da Computação"
+            , "level": "G"
+            , "area": "Exatas"
+            , "qualification": "A"
+            , "credits": -1
+        };
+        request(app)
+            .post('/courses')
+            .send(course)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(500)
+            .end(function(error, response) {
+                if (error) {
+                    next(error);
+                } else {
+                    next();
+                }
+            });
+    });
+
     it('should create with valid credentials', function (next) {
         var course = {
             "id": "42"
@@ -47,7 +88,14 @@ describe('list course', function () {
             .get('/courses')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
-            .expect(200, next);
+            .expect(200)
+            .end(function(error, response) {
+                if (error) {
+                    next(error);
+                } else {
+                    next();
+                }
+            });
     });
 });
 
@@ -57,9 +105,54 @@ describe('show course', function () {
         next();
     });
 
+    it('should not find the course', function (next) {
+        request(app)
+            .get('/courses/5324c3d7a26c43d84defde12')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /plain/)
+            .expect(404)
+            .end(function(error, response) {
+                if (error) {
+                    next(error);
+                } else {
+                    next();
+                }
+            });
+    });
+
     it('should show', function (next) {
-        // TODO
-        next();
+        var course = {
+            "id": "42"
+            , "name": "Ciência da Computação"
+            , "level": "G"
+            , "area": "Exatas"
+            , "qualification": "A"
+            , "credits": 200
+        };
+        request(app)
+            .post('/courses')
+            .send(course)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(error, response) {
+                if (error) {
+                    next(error);
+                } else {
+                    request(app)
+                        .get('/courses/' + response.body._id)
+                        .set('Accept', 'application/json')
+                        .expect('Content-Type', /json/)
+                        .expect(200)
+                        .end(function(error, response) {
+                            if (error) {
+                                next(error);
+                            } else {
+                                next();
+                            }
+                        });
+                }
+            });
     });
 });
 
@@ -69,9 +162,63 @@ describe('update course', function () {
         next();
     });
 
+    it('should not find the course', function (next) {
+        request(app)
+            .put('/courses/5324c3d7a26c43d84defde12')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /plain/)
+            .expect(404).end(function(error, response) {
+                if (error) {
+                    next(error);
+                } else {
+                    next();
+                }
+            });
+    });
+
     it('should update', function (next) {
-        // TODO
-        next();
+        var course = {
+            "id": "42"
+            , "name": "Ciência da Computação"
+            , "level": "G"
+            , "area": "Exatas"
+            , "qualification": "A"
+            , "credits": 200
+        };
+        request(app)
+            .post('/courses')
+            .send(course)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(error, response) {
+                if (error) {
+                    next(error);
+                } else {
+                    var couser = {
+                        "id": "43"
+                        , "name": "Ciência da Computação 2"
+                        , "level": "G 2"
+                        , "area": "Exatas 2"
+                        , "qualification": "A 2"
+                        , "credits": 300
+                    };
+
+                    request(app)
+                        .put('/courses/' + response.body._id)
+                        .send(couser)
+                        .set('Accept', 'application/json')
+                        .expect('Content-Type', /json/)
+                        .expect(200)
+                        .end(function(error, response) {
+                            if (error) {
+                                next(error);
+                            } else {
+                                next();
+                            }
+                        });
+                }
+            });
     });
 });
 
@@ -81,8 +228,53 @@ describe('remove course', function () {
         next();
     });
 
+    it('should not find the course', function (next) {
+        request(app)
+            .del('/courses/5324c3d7a26c43d84defde12')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /plain/)
+            .expect(404)
+            .end(function(error, response) {
+                if (error) {
+                    next(error);
+                } else {
+                    next();
+                }
+            });
+    });
+
     it('should remove', function (next) {
-        // TODO
-        next();
+        var course = {
+            "id": "42"
+            , "name": "Ciência da Computação"
+            , "level": "G"
+            , "area": "Exatas"
+            , "qualification": "A"
+            , "credits": 200
+        };
+        request(app)
+            .post('/courses')
+            .send(course)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(error, response) {
+                if (error) {
+                    next(error);
+                } else {
+                    request(app)
+                        .del('/courses/' + response.body._id)
+                        .set('Accept', 'application/json')
+                        .expect('Content-Type', /json/)
+                        .expect(200)
+                        .end(function(error, response) {
+                            if (error) {
+                                next(error);
+                            } else {
+                                next();
+                            }
+                        });
+                }
+            });
     });
 });
